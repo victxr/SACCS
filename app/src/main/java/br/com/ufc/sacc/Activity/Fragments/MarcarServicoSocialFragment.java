dc5c5f9d83575d19b532c7d152e21b32fef12bea
Victor Jeronimo Age <40958805+victxr@users.noreply.github.com>
2018-11-23 22:33:53 -0300

    Faltando somente persistir.

---
+++ app/src/main/java/br/com/ufc/sacc/Activity/Fragments/MarcarServicoSocialFragment.java
@@ -0,0 +1,30 @@
+package br.com.ufc.sacc.Activity.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import br.com.ufc.sacc.Activity.Adapter.ExpandableListAdapter;
+import br.com.ufc.sacc.DAO.ConfiguracaoFirebase;
+import br.com.ufc.sacc.Model.ItemFaq;
+import br.com.ufc.sacc.R;
+import com.google.firebase.database.*;
+
+import java.util.ArrayList;
+
+public class MarcarServicoSocialFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_servico_social_marcar, null);
+
+        return view;
+    }
+}
--- app/src/main/java/br/com/ufc/sacc/Activity/MarcarConsultaActivity.java
+++ app/src/main/java/br/com/ufc/sacc/Activity/MarcarConsultaActivity.java
@@ -2,19 +2,58 @@ package br.com.ufc.sacc.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
+import br.com.ufc.sacc.Activity.Fragments.*;
 import br.com.ufc.sacc.R;
 
-public class MarcarConsultaActivity extends AppCompatActivity {
+public class MarcarConsultaActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marcar);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.item_psicologa);
 
+        loadFragment(new MarcarPsicologaFragment());
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch(menuItem.getItemId()){
+            case R.id.item_psicologa:
+                fragment = new MarcarPsicologaFragment();
+                break;
+
+            case R.id.item_nutricionista:
+                fragment = new MarcarNutricionistaFragment();
+                break;
+
+            case R.id.item_servico_social:
+                fragment = new MarcarServicoSocialFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
