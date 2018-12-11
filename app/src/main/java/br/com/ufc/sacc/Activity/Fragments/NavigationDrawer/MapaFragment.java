6a06c06497f513c112c397c5ec1da276469b13a6
Jonas Lopes <jonaslopesdoo@hotmail.com>
2018-12-11 11:43:58 -0300

    Tela de mapa rodando

--- app/src/main/java/br/com/ufc/sacc/Activity/Fragments/NavigationDrawer/MapaFragment.java
+++ app/src/main/java/br/com/ufc/sacc/Activity/Fragments/NavigationDrawer/MapaFragment.java
@@ -5,6 +5,7 @@ import android.location.Geocoder;
 import android.os.Bundle;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,23 +22,28 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
     public void onMapReady(GoogleMap googleMap) {
 
         LatLng posicaoUfc = getCoordenadaEndereco("Av. José de Freitas Queiroz, 5003 - Cedro, Quixadá - CE, 63900-000");
-        if(posicaoUfc != null){
+        if (posicaoUfc != null) {
             CameraUpdate update = CameraUpdateFactory.newLatLngZoom(posicaoUfc, 17);
             googleMap.moveCamera(update);
+            MarkerOptions pino = new MarkerOptions();
+            pino.position(posicaoUfc);
+            pino.title("UFC - Universidade Federal do Ceará");
+            pino.snippet("Campus Quixadá");
+            googleMap.addMarker(pino);
         }
     }
 
-    private LatLng getCoordenadaEndereco(String endereco){
+    private LatLng getCoordenadaEndereco(String endereco) {
         try {
             Geocoder geocoder = new Geocoder(getContext());
             List<Address> resultadosEndereco =
                     geocoder.getFromLocationName(endereco, 1);
 
-            if(!resultadosEndereco.isEmpty()){
+            if (!resultadosEndereco.isEmpty()) {
                 LatLng posicao = new LatLng(resultadosEndereco.get(0).getLatitude(), resultadosEndereco.get(0).getLongitude());
                 return posicao;
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
